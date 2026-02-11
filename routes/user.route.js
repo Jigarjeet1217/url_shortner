c1eacda57eb2793ed2b43384e599607d1833f21a
JigarjSi <jigarjeet.singh@rsgmedia.com>
2026-02-11 11:32:50 +0530

    logout route added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
-const { setCookies } = require('../common/cookies');
 const ResponseController = require('../common/ResponseController');
 const { isAuthorized } = require('../middlewares/authenticated');
 const Router = express.Router();
@@ -11,6 +10,17 @@ Router.post('/signup', isAuthorized, async (req, res, next) => {
 		let UC = new UserController();
 		let data = await UC.signupUser(req.body);
 		RC.success(res, { message: 'User Created', data, isNew: true })
+	} catch (error) {
+		RC.failed(res, error);
+	}
+	next();
+}, ResponseController.sendResponse)
+
+Router.get('/logout', async (req, res, next) => {
+	let RC = new ResponseController();
+	try {
+		res.clearCookie('Authorization');
+		RC.success(res, { message: 'User logged out successfully!!!', data: null, isNew: false })
 	} catch (error) {
 		RC.failed(res, error);
 	}
